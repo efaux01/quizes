68097d41ef04403d9868ce0da45385e48948ca75
efaux01 <eafaux@gmail.com>
2018-09-24 20:57:33 -0400

    Added multiple choice randomizer, display, & correct check

--- src/quizes/DistractionQuiz.java
+++ src/quizes/DistractionQuiz.java
@@ -5,65 +5,126 @@
  */
 package quizes;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 /**
  *
  * @author christopher.eckles
  */
 public class DistractionQuiz {
-
-    private static String wordBank[] = {"String", "String"};
+    private static Map<String, Map<String, String>> statementMap = new HashMap<>();
+    private static Map<String, String> questionMap = new HashMap<>();
+    
+    
+        
+        
+    private static final String WORDBANK[] = {"whale", "shark", "sheep","turkey",
+        "donkey","dog","cat","pigeon","tiger","elephant","crow","lion","bear",
+    "fox","wolf","monkey","peacock","panda","deer","frog","apron","vest",
+    "nose","toes","mouth","apple","orange","grapes","cheese","beach","city",
+    "town","mountain","lake","forest","school","pencil","rock","tree",
+    "computer","book","chair","towel","spoon","arrow","pen","pencil","food",
+    "seeds","coconut","chocolate","flower","ball","doll","girl","man","boy",
+    "woman", "dolphin", "home", "bed"};
 
     public static void main(String[] args) {
-        Map<String, Map<String, String>> statementMap = new HashMap<>();
-        Map<String, String> questionMap = new HashMap<>();
-        questionMap.put("Question1", "CorrectAnswer1");
-        questionMap.put("Question2", "CorrectAnswer2");
+        statement1();
+    }
+                
+    public static void statement1(){
+        
 
-        questionMap = new HashMap<>();
-        questionMap.put("Question3", "CorrectAnswer3");
-        questionMap.put("Question4", "CorrectAnswer4");
-
-        statementMap.put("This is the first statement", questionMap);
-
-        questionMap = new HashMap<>();
+        statementMap.put("Rusy, the big, brown dog went to the beach on sunday. "
+                + "When he was there, \nhe met a little girl who was playing with "
+                + "a ball. He really wanted to play \nwith her, but he was too "
+                + "big and scared her. Rusty went down to the water \nto find "
+                + "another friend. Look! He saw a whale searching for food in "
+                + "the \nwater. Rusty was hoping to find a dolphin friend but "
+                + "instead there is a \nshark! Rusty thinks the beach is scary "
+                + "and wants to go home and find a \ntowel. Hopefully the cat "
+                + "hasn't stolen his bed again.\n", questionMap);
+        
+        questionMap.clear();
+        questionMap.put("girl", "ball");
+        questionMap.put("whale", "food");
+        questionMap.put("shark","dolphin");
+        questionMap.put("cat","bed");
+        
+    }//close statement1
+    
+    public static void statement2(){
+        statementMap.put("Statement2", questionMap);
+    
+        questionMap.clear();
         questionMap.put("Question5", "CorrectAnswer5");
         questionMap.put("Question6", "CorrectAnswer6");
-
-        statementMap.put("This is the second statement", questionMap);
-
-        for (Map.Entry<String, Map<String, String>> entry : statementMap.entrySet()) {
+        questionMap.put("Question7", "CorrectAnswer7");
+        questionMap.put("Question8", "CorrectAnswer8");
+    }//close statement2
+    
+    public static void statement3(){
+        statementMap.put("Statement 3", questionMap);
+        
+        questionMap.clear();
+        questionMap.put("Question9", "CorrectAnswer9");
+        questionMap.put("Question10", "CorrectAnswer10");
+        questionMap.put("Question11", "CorrectAnswer11");
+        questionMap.put("Question12", "CorrectAnswer12");
+    }//close statement3
+        
+    
+    /*for (Map.Entry<String, Map<String, String>> entry : statementMap.entrySet()) {
             System.out.println(entry.getKey());
-            //time delay
             for (Map.Entry<String, String> questions : entry.getValue().entrySet()) {
-                System.out.println("What word was associated with " + questions.getKey() + "?  Answer:" + questions.getValue());
+                System.out.println("What word was associated with " + questions.getKey() + "?");
                 generatePossibleAnswers(questions.getValue());
             }
         }
-    }
+    }*/
+    
+    
     private static void generatePossibleAnswers(String correctAnswer){
-        //randomize until 4 answers are generated
-        //print the 4 answers with either numbers or lettesr
+        //randomize until 4 answers are generated for multiple choice list
         ArrayList<String> answers = new ArrayList<>();
            answers.add(correctAnswer);
         while(answers.size() < 4){
-        int rand = RandomNumber.generateRandomLocation(wordBank.length);
-        boolean add = true;
-        String answerToAdd = wordBank[rand];
-        for(String answer: answers){
-            if(answer.equals(answerToAdd))
-                add = false;
-        }
-        if(add){
-          answers.add(wordBank[rand]);
-        }
-        }
-        //todo  while(answers.size)
+            int rand = RandomNumber.generateRandomLocation(WORDBANK.length);
+            boolean add = true;
+            String answerToAdd = WORDBANK[rand];
+            for(String answer: answers){
+                if(answer.equals(answerToAdd)){
+                    add = false;
+                }//close if
+            }//close for
+            if(add){
+                answers.add(WORDBANK[rand]);
+            }//close if
+        }//close while loop
         
+        //shuffle answer order
+        Collections.shuffle(answers);
+        ListIterator<String> iter = answers.listIterator(0);
+        
+        //display list of multiple choice answers
+        while(iter.hasNext()){
+            System.out.println(iter.nextIndex()+1 +"." + iter.next());
+        }//close while loop
+        
+        //collect user answer
+        Scanner input = new Scanner(System.in);
+        int userChoice = input.nextInt()-1;
+        
+        //check for correct answer
+        if(answers.get(userChoice).equals(correctAnswer)){
+            System.out.println("Correct!");
+        }else{System.out.println("Incorrect");
+        }//close if/else
         
     }
     
